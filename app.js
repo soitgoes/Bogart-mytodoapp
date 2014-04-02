41a284176a0e04ff2790ff75a0aef4210014f1e2
mcsharps <madisonsharps@gmail.com>
2014-04-02 16:17:54 -0500

    took out express stuff that was broken, started messing with mustache templates, should be working

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-var bogart = require('bogart');
-var viewEngine = bogart.viewEngine('mustache');
+var bogart = require('bogart')
+, path   = require('path');
+
+var viewEngine = bogart.viewEngine('mustache', path.join(bogart.maindir(), 'views'));
+
 var router = bogart.router();
-var mysql      = require('mysql');
-var settings = require('./settings.js')
-var fs = require('fs'); //is this bogart friendly?
-var db_helper = require("./mysql_db_notes.js");
-connection.connect();
-//probably need to remove this excess boilerplate msyql code and get the mysql_db_notes integrated
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
 
-  console.log('The solution is: ', rows[0].solution);
-});
+var context = {
+	locals: {
+		ListTitle: "My Todo List",
+		TodoItems: [
+					{value: 'This is my first item to do!'}, 
+					{value: 'This is my second item to do!'},
+					{value: 'This is my third item to do!'}
+					]
+	},
+};
 
-connection.end();
-var post  = {id: 1, title: 'Hello MySQL'};
-var query = connection.query('INSERT INTO posts SET ?', post, function(err, result) {
-  // Neat!
+router.get('/', function(req, res) {
+  return viewEngine.respond('index.html', context);
+  // Mustache.to_html(templateString, context.locals)
 });
-console.log(query.sql); // INSERT INTO posts SET `id` = 1, `title` = 'Hello MySQL'
-router.get('/', function(req) {
-  return viewEngine.respond('index.html', {
-    locals: {}
-  });
-});
+function SaveText(){
+var todotext = $("#TextToSave").val();
+context.TodoItems.push({value: "" + todotext + ""});
+};
 var app = bogart.app();
-app.use(bogart.batteries); // A batteries included JSGI stack including streaming request body parsing, session, flash, and much more.
+
 app.use(router); // Our router
 
 app.start();
